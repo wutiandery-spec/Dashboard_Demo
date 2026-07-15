ce919c4a63cc9c1775504257bc980da7d6a3c7c9
miaomiaoe <wutiandery@gmail.com>
2026-07-15 14:54:25 +0800

    Vue3-Store数据仓库

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+<count/>
+<br>
+<LoveTalk/>
+</template>
+<script lang="ts" setup name="App">
+import count from '@/components/Count.vue';
+import LoveTalk from '@/components/LoveTalk.vue';
+</script>
+<style scoped>
+
+</style>
